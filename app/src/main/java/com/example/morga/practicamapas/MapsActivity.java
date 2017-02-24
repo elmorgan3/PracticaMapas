19bfb64694ab68d6090c24463c4b8d16252ef7cc
Morgan <morgan.cordoba@hotmail.com>
2017-02-24 14:00:04 +0100

    Guardada las imagenes de los sitos
    y el navigation drawer y el map

---
+++ app/src/main/java/com/example/morga/practicamapas/MapsActivity.java
@@ -0,0 +1,61 @@
+package com.example.morga.practicamapas;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    private Marker marcador;
+    double latitud = 0.0;
+    double longitud = 0.0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    private void AddMarcadorMapa(double lat, double lng) {
+        LatLng coordenades = new LatLng(lat, lng);
+        CameraUpdate mevaUbicacio = CameraUpdateFactory.newLatLngZoom(coordenades, 16);
+        if (marcador != null) marcador.remove();
+        marcador = mMap.addMarker(new MarkerOptions()
+                .position(coordenades)
+                .title("Escola Pia Mataró")
+                .icon(BitmapDescriptorFactory.fromResource((R.mipmap.ic_launcher))));
+        mMap.animateCamera(mevaUbicacio);
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        AddMarcadorMapa(40.7,-74.00);
+    }
+}
---
+++ app/src/main/java/com/example/morga/practicamapas/Monumento.java
@@ -0,0 +1,30 @@
+package com.example.morga.practicamapas;
+
+/**
+ * Created by morga on 24/02/2017.
+ */
+
+public class Monumento {
+    private String titulo;
+    private String descripcion;
+    private String imagen;
+
+    public Monumento (String tit, String des, String im)
+    {
+        titulo=tit;
+        descripcion=des;
+        imagen=im;
+    }
+
+    public String getTitulo()
+    {
+        return titulo;
+    }
+
+    public String getSubtitulo()
+    {
+        return descripcion;
+    }
+
+    public String getImagen() { return imagen; }
+}
